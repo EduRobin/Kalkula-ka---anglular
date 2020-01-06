1157c9f42b06f28c3b7647a0f8c9ec92a34167de
EduRobin <43910646+EduRobin@users.noreply.github.com>
2020-01-06 12:15:57 +0100

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  public result;
+
+  nula() {
+    this.result += 0;
+  }
+  jedna() {
+    this.result += 1;
+  }
+  dva() {
+    this.result += 2;
+  }
+  tri() {
+    this.result += 3;
+  }
+  ctyri() {
+    this.result += 4;
+  }
+  pet() {
+    this.result += 5;
+  }
+  sest() {
+    this.result += 6;
+  }
+  sedm() {
+    this.result += 7;
+  }
+  osm() {
+    this.result += 8;
+  }
+  devet() {
+    this.result += 9;
+  }
+
+  minus() {
+    this.result +=  '-';
+  }
+  plus() {
+    this.result += '+';
+  }
+
+  deleno() {
+    this.result += '/';
+  }
+
+  krat() {
+    this.result += '*';
+  }
+
+  vysledek() {
+     const expression = this.result;
+    // tslint:disable-next-line:no-eval
+     this.vysledek = eval(expression.toString());
+  }
+
+
+
+}
+
+
+
+
+
